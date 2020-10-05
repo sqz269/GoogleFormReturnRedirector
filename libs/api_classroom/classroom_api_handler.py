b5b776833b2efddcc23682f8b8b47ef50bf1aa85
sqz269 <yin2690855381@gmail.com>
2020-10-05 08:44:08 -0500

    Added some api for classroom_api_handler.py

--- libs/api_classroom/classroom_api_handler.py
+++ libs/api_classroom/classroom_api_handler.py
@@ -3,6 +3,12 @@ from logging import Logger
 from googleapiclient.discovery import build
 from libs.api_shared.credential_manger import CredentialManager
 
+class CourseState:
+    ACTIVE = "ACTIVE"
+    ARCHIVED = "ARCHIVED"
+    PROVISIONED = "PROVISIONED"
+    DECLINED = "DECLINED"
+    SUSPENDED = "SUSPENDED"
 
 class ClassroomAPIHandler(CredentialManager):
     def __init__(self, logger: Logger, token_store_path: str = "",
@@ -13,3 +19,17 @@ class ClassroomAPIHandler(CredentialManager):
 
     def auth_service(self) -> None:
         self.service = build('classroom', 'v1', credentials=self.credentials)
+
+    def list_classroom(self, teacher_id="me", student_id="",
+                       course_state: CourseState = CourseState.ACTIVE,
+                       page_size: int = 20, page_token: str = ""):
+        return self.service.courses().list(teacherId=teacher_id, studentId=student_id,
+                                           courseStates=course_state,
+                                           pageSize=page_size, pageToken=page_token).execute()
+
+    def list_students(self, course_id: str, page_size: int = 50, page_token: str = ""):
+        return self.service.courses().students().list(courseId=course_id,
+                                                      pageSize=page_size, pageToken=page_token).execute()
+
+    def get_student(self, course_id: str, user_id: str = "me"):
+        return self.service.courses().students().get(courseId=course_id, userId=user_id).execute()
